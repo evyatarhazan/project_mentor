7f5c3f9ea4738f38f5355a043178a1ed82109c19
evyatarhazan <7127547@gmail.com>
2023-05-01 09:47:09 +0300

    add composite PK for StudentGroupRelationship

--- server/models/studentGroupRelationship/type.js
+++ server/models/studentGroupRelationship/type.js
@@ -8,21 +8,32 @@ const StudentGroupRelationship = db.define('StudentGroupRelationship', {
     student_id: {
         type: INTEGER,
         allowNull: false,
+        primaryKey: true,
         references: {
             model: Students,
             key: 'id'
-        }
+        },
+        unique: 'compositeIndex' 
     },
     group_id: {
         type: INTEGER,
         allowNull: false,
+        primaryKey: true,
         references: {
             model: Groups,
             key: 'id'
-        }
+        },
+        unique: 'compositeIndex' 
     }
+}, {
+    indexes: [
+        {
+            name: 'compositeIndex',
+            unique: true,
+            fields: ['student_id', 'group_id']
+        }
+    ]
 });
-
 
 export default StudentGroupRelationship;
 
